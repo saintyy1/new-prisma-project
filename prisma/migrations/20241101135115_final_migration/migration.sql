a14aa664de71ba8d08af9aaed0210727438a1490
tolu <olufowobirasaq69@gmail.com>
2024-11-06 16:56:32 +0100

    migrated

--- prisma/migrations/20241101135115_final_migration/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Made the column `verificationCode` on table `user` required. This step will fail if there are existing NULL values in that column.
-
-*/
--- AlterTable
-ALTER TABLE `user` MODIFY `verificationCode` VARCHAR(191) NOT NULL;
--- prisma/migrations/20241104135604_add_gender_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [OTHER] on the enum `User_gender` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `user` MODIFY `gender` ENUM('MALE', 'FEMALE') NOT NULL;
